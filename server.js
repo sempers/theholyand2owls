a3865ed70b09850a9878e34d991f0c702aa77b5e
sempers <groddenator@gmail.com>
2014-06-23 22:52:06 +0400

    final

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.engine('html', ejs.renderFile);
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/public');
 app.set('view engine', 'ejs');
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
 
 app.get('/*', function(req, res){
     res.render('index.html');
